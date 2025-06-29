b393d9713c41a95a5791af07cd6387f2132b23af
Teja3376 <kcteja62892@gmail.com>
2025-06-29 15:41:02 +0530

    initial commit

---
+++ backend/database/dbconfig.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+function database () {
+    const mongoURL =  process.env.MONGO_URL || 'mongodb+srv://kcteja62892:Teja%402004@cluster0.plavaod.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+    mongoose.connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+}
+module.exports = database;
